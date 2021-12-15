255c6513cde0894a77ee905370ba714783488d33
David-Lisboa <dlrlisboa@gmail.com>
2021-12-14 23:04:04 -0400

    atalhos

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Bom dia");
+
+    }
+}
+//psvm - public static void main(String[] args){}
+//sout - System.out.println();
